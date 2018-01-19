5ed6aff3cf775bf2079b611637f73fbaa59f67fd
Nirgiemand <henri.tamvere@khk.ee>
2018-01-19 10:30:01 +0200

    muutujate väärtuste ja tühiväärtuste sisestamine

---
+++ model/template.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henri.tamvere
+ * Date: 19.01.2018
+ * Time: 10:22
+ */
+
+class template
+{
+    // klassi muutujad
+    var $file = ''; // HTML malli faili nimi
+    var $content = false; // HTML malli sisu, mis alguses puudub, HTML failist loetud sisu
+    var $vars = array (); // HTML malli elementide nimetuste ja reaalväärtuste paarid
+
+}
