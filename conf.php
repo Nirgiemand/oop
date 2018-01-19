8a3226db73172a30f2104b8dbf2a9c22610ecd2c
Nirgiemand <henri.tamvere@khk.ee>
2018-01-19 09:50:40 +0200

    projekti kaustaga seotud abikonstantide määramine

---
+++ conf.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henri.tamvere
+ * Date: 19.01.2018
+ * Time: 9:44
+ */
+
+// kaustade ja failide konstantsed nimetused
+define('MODEL_DIR', 'model/');
+define('VIEW_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
