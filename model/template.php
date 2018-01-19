74c31ef46e0cefaaf386be45b6b8c27b271e878c
Nirgiemand <henri.tamvere@khk.ee>
2018-01-19 10:37:44 +0200

    HTML faili sisu lugemine on kirjeldatud

--- model/template.php
+++ model/template.php
@@ -13,4 +13,12 @@ class template
     var $content = false; // HTML malli sisu, mis alguses puudub, HTML failist loetud sisu
     var $vars = array (); // HTML malli elementide nimetuste ja reaalväärtuste paarid
 
+    // HTML failist sisu lugemine
+    function readFile($file) {
+        /*
+         $fp = fopen($file, 'r');
+        $this->content = fread($fp,filesize($file));
+        fclose($fp); */
+        $this->content = file_get_contents($file)
+    }
 }
