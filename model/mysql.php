10d98c8041ccfbae1fedd8fab59c4bc7a397da68
Nirgiemand <henri.tamvere@khk.ee>
2018-02-02 11:00:15 +0200

    kirjeldati andmebaasi objekti loomist

---
+++ model/mysql.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henri.tamvere
+ * Date: 2.02.2018
+ * Time: 10:49
+ */
+
+class mysql
+{
+    // klassi väljad
+    var $conn = false; // ühendus db serveriga
+    var $host = false; // db serveri nim/ip
+    var $user = false; // db  kasutaja nimi
+    var $pass = false; // db kasutaja parool
+    var $dbname = false; // db nimi
+
+    /**
+     * mysql constructor.
+     * @param bool $host
+     * @param bool $user
+     * @param bool $pass
+     * @param bool $dbname
+     */
+    public function __construct($host, $user, $pass, $dbname)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->dbname = $dbname;
+        $this->connect();
+    }
+    function connect() {
+        $this->conn=mysqli_connect($this->host, $this->user, $this->pass,$this->dbname);
+        if($this->conn == false) {
+            echo 'Probleem andmebaasi ühendamisega <br />';
+            exit;
+        }
+    }
+
+
+}
