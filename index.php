6c8b8ace6c792807b70473b2224a7f0ac2480023
Nirgiemand <henri.tamvere@khk.ee>
2018-02-02 10:24:56 +0200

    index täiendatud

--- index.php
+++ index.php
@@ -2,21 +2,23 @@
 /**
  * Created by PhpStorm.
  * User: henri.tamvere
- * Date: 19.01.2018
- * Time: 11:09
+ * Date: 31.01.2018
+ * Time: 08:40
  */
 // loeme sisse projekti konfiguratsiooni
 require_once 'conf.php';
-
-// loome test objekti template klassist
-$testTabel = new template('views/test.html');
-
-//määrame reaaalväärtused
-$testTabel->set('esimene','1');
-$testTabel->set('teine','2');
-
-// lisame objekti testvaade
-echo '<pre>';
-print_r($testTabel);
-echo '<pre />';
-$testTabel->parse();
+// loome peamalli objekti template klassist
+$mainTmpl = new template('main');
+// lubame kontrollerite haldust
+require_once 'control.php';
+// määrame reaalväärtused malli elementidele
+$mainTmpl->set('lang', 'et');
+$mainTmpl->set('page_title', 'Lehe pealkiri');
+$mainTmpl->set('user', 'Kasutaja');
+$mainTmpl->set('title', 'Pealkiri');
+$mainTmpl->set('lang_bar', 'Keeleriba');
+// katsetame menüü loomist
+require_once 'menu.php';
+echo $mainTmpl->parse();
+// kontrollime control sisu
+echo $http->get('control');
