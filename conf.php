66f1062804a920a40408671e7f16782ad23b05f9
Nirgiemand <henri.tamvere@khk.ee>
2018-02-02 10:40:22 +0200

    õige conf

--- conf.php
+++ conf.php
@@ -10,13 +10,13 @@
 define('MODEL_DIR', 'model/');
 define('VIEW_DIR', 'views/');
 define('CONTROL_DIR', 'controllers/');
-define('LIB_DIR', 'lib/');
+define('LIB_DIR', 'libs/');
 define('DEFAULT_CONTROL', 'default'); // vaikimis kasutatav kontroller
 // nõuame abifunktisoonide faili kasutamist
 require_once LIB_DIR.'utils.php';
 // nõuame vajalike failide kasutamist
 require_once MODEL_DIR.'template.php';
 require_once MODEL_DIR.'http.php';
-require_once MODEL_DIR.'linkobject.php';
+require_once MODEL_DIR.'linkobjects.php';
 // loome objektid, mis oleks vaja pidevalt kasutada
 $http = new linkobject();
