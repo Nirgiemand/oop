8731d86fe6c18e6d06876132544fb63a6def7967
Nirgiemand <henri.tamvere@khk.ee>
2018-02-02 10:44:17 +0200

    lisatud avalehe link

--- menu.php
+++ menu.php
@@ -10,22 +10,29 @@ $menuTmpl = new template('menu.menu');
 // loome menüü elemendi malli objekti
 $menuItemTmpl = new template('menu.menu_item');
 // ###############################################
+//AVALEHT
+$menuItemTmpl->set('menu_item_name', 'Avaleht');
+//loome avalehe lingi
+$link = $http->getLink(array('control'=>'default'));
+$menuItemTmpl->set('menu_item_url',$link);
+// täidame loodud elemendiga lehe menüü
+$menuItem = $menuItemTmpl->parse();
+$menuTmpl->add('menu_items', $menuItem);
 // tegutseme ühe menüü elemendiga
 // esimene
 $menuItemTmpl->set('menu_item_name', 'esimene');
 // loome lingi
 $link = $http->getLink(array('control' => 'esimene'));
-$menuItemTmpl->set('link', $link);
+$menuItemTmpl->set('menu_item_url', $link);
 // täidame loodud elemendiga lehe menüü
 $menuItem = $menuItemTmpl->parse();
 $menuTmpl->add('menu_items', $menuItem);
-// ###############################################
 // tegutseme ühe menüü elemendiga
 // teine
 $menuItemTmpl->set('menu_item_name', 'teine');
 // loome lingi
 $link = $http->getLink(array('control' => 'teine'));
-$menuItemTmpl->set('link', $link);
+$menuItemTmpl->set('menu_item_url', $link);
 // täidame loodud elemendiga lehe menüü
 $menuItem = $menuItemTmpl->parse();
 $menuTmpl->add('menu_items', $menuItem);
