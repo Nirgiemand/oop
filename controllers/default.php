a40bf2f61716cd4978eb2eecb90ab8f902f0ac61
Nirgiemand <henri.tamvere@khk.ee>
2018-02-06 11:01:24 +0200

    koostati vaikimisi kontrolleri andmebaasi andmete töötlus

--- controllers/default.php
+++ controllers/default.php
@@ -5,4 +5,20 @@
  * Date: 2.02.2018
  * Time: 10:31
  */
-$mainTmpl->set('content', 'Lehe vaikimisi sisu');
+$page_id = (int)$http->get('page_id'); // lehe id
+// lehe id järgi küsime sisu andmebaasist
+$sql = 'SELECT content FROM content '.
+    'WHERE content_id='.fixDb($page_id);
+$result = $db->getData($sql);
+// kui vastavalt page_id-le ei leidu andmebaasis vastust
+if($result == false) {
+    // siis pöördume avalehele - is_first_page = "1"
+    $sql = 'SELECT * FROM content WHERE '.
+        'is_first_page='.fixDb(1);
+    $result = $db->getData($sql);
+}
+if($result != false) {
+    // siis tulemus koosneb ainult 1-st reast - see ongi vastava lehe sisu
+    $page = $result[0];
+    $mainTmpl->set('content', $page['content']);
+}
