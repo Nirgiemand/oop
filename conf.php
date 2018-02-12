da5a2ee931d06cce2b0155ec1597f2511065c21b
Nirgiemand <henri.tamvere@khk.ee>
2018-02-12 08:53:47 +0200

    lisati erinevate rollide konstandid

--- conf.php
+++ conf.php
@@ -14,6 +14,12 @@ define('LIB_DIR', 'libs/');
 define('DEFAULT_CONTROL', 'default'); // vaikimis kasutatav kontroller
 // nõuame abifunktisoonide faili kasutamist
 require_once LIB_DIR.'utils.php';
+
+// määrame erinevad rollid
+define('ROLE_NONE', 0);
+define('ROLE_USER', 1);
+define('ROLE_ADMIN', 2);
+
 // nõuame vajalike failide kasutamist
 require_once MODEL_DIR.'template.php';
 require_once MODEL_DIR.'http.php';
